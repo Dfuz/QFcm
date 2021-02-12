e79b782779fb0a2e87a523387ca1cf964f03322b
Gleb Lysov <lysovg9@gmail.com>
2021-02-12 04:07:41 +0300

    fix prev

--- build/test/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/pilo/proj/vadim/QFcm/test
-# Build directory: /home/pilo/proj/vadim/QFcm/build/test
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(fcmanager "/home/pilo/proj/vadim/QFcm/build/test/fcmanager")
-set_tests_properties(fcmanager PROPERTIES  _BACKTRACE_TRIPLES "/home/pilo/proj/vadim/QFcm/test/CMakeLists.txt;16;add_test;/home/pilo/proj/vadim/QFcm/test/CMakeLists.txt;0;")
